9d9aad4ab3bd9fe1b4bbc8bda9f85e5f166f9e6e
César <isamuhikaru@hotmail.com>
2018-01-23 16:35:19 -0600

    Creacion de Objeto/Metodo

--- js/app.js
+++ js/app.js
@@ -1,19 +1,30 @@
-var teclas = document.getElementsByClassName("tecla"); 
+var calculadora = {
+  interactividad_botones:function(){
+    var teclas = document.getElementsByClassName("tecla"); 
 
-var boton_apretado = function(){
-   this.style.transform = "scale(0.95,0.95)"; 
+    var boton_apretado = function(){
+       this.style.transform = "scale(0.95,0.95)"; 
+    };
+
+    //Este ciclo reparte la funcionalidad a todas las teclas:  
+      for (var i = 0; i < teclas.length; i++) {
+        teclas[i].addEventListener("mousedown", boton_apretado);
+      };
+
+    var boton_soltado = function(){
+      this.style.transform = "scale(1,1)";
+    };
+
+    //Este ciclo reparte la funcionalidad a todas las teclas: 
+    for (var i = 0; i < teclas.length; i++) {
+        teclas[i].addEventListener("mouseup", boton_soltado);
+      };
+  }//aqui va la coma
 };
 
-//Este ciclo reparte la funcionalidad a todas las teclas:  
-  for (var i = 0; i < teclas.length; i++) {
-    teclas[i].addEventListener("mousedown", boton_apretado);
-  };
+calculadora.interactividad_botones();
 
-var boton_soltado = function(){
-  this.style.transform = "scale(1,1)";
-};
 
-//Este ciclo reparte la funcionalidad a todas las teclas: 
-for (var i = 0; i < teclas.length; i++) {
-    teclas[i].addEventListener("mouseup", boton_soltado);
-  };
+
+
+
